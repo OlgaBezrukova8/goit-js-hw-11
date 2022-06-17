00fdc8f4147da083b742dc6cb50ca57c6f934db0
OlgaBezrukova8 <olgabezrukova888@gmail.com>
2022-06-17 17:21:01 +0100

    Added pagination cards

--- src/fetchCards.js
+++ src/fetchCards.js
@@ -3,13 +3,40 @@ import Axios from 'axios';
 const API_KEY = '28090612-053d38b519fb99dbfe43ba7b5';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchCards(name) {
-  const response = await fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  const card = await response.json();
-  return card;
-  //   return fetch(
-  //     `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-  //   ).then(res => res.json());
+export default class CardsApiService {
+  constructor() {
+    console.log(this);
+    this.searchBar = '';
+    this.page = 1;
+    this.per_page = 40;
+  }
+
+  async fetchCards() {
+    const response = await fetch(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchBar}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`
+    );
+    const card = await response.json();
+    this.incrementPage();
+
+    return card;
+    //   return fetch(
+    //     `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
+    //   ).then(res => res.json());
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get search() {
+    return this.searchBar;
+  }
+
+  set search(newSearch) {
+    this.searchBar += newSearch;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './css/styles.css';
+import CardsApiService from './fetchCards';
+import Axios from 'axios';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import Axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { fetchCards } from './fetchCards';
 
 // const API_KEY = '28090612-053d38b519fb99dbfe43ba7b5';
 // const BASE_URL = 'https://pixabay.com/api/';
@@ -13,21 +13,42 @@ import { fetchCards } from './fetchCards';
 const $refs = {
   form: document.querySelector('#search-form'),
   container: document.querySelector('.gallery'),
+  loadButton: document.querySelector('.load-more'),
 };
 
 $refs.form.addEventListener('submit', onSubmitForm);
+$refs.loadButton.addEventListener('click', onLoadMore);
+
+const cardsApiService = new CardsApiService();
+$refs.loadButton.classList.add('is-hidden');
+// $refs.loadButton.style.display = 'none';
 
 function onSubmitForm(event) {
   event.preventDefault();
 
-  const searchImageName = $refs.form.searchQuery.value;
-  const searchImageNameTrim = searchImageName.trim();
+  cardsApiService.searchBar = $refs.form.searchQuery.value;
+  if (cardsApiService.searchBar === '') {
+    Notify.failure('Enter the search value!');
+    return;
+  }
 
-  fetchCards(searchImageNameTrim).then(renderCardOfList);
+  cardsApiService.resetPage();
+  cardsApiService.fetchCards().then(hits => {
+    clearCardsContainer();
+    renderCardOfList(hits);
+    $refs.loadButton.classList.remove('is-hidden');
+  });
+}
+
+function onLoadMore() {
+  cardsApiService.fetchCards().then(renderCardOfList);
 }
 
 function renderCardOfList(images) {
-  if (images.total === 0) {
+  // console.log(images.totalHits);
+  // console.log(images.hits.length);
+
+  if (images.hits.length === 0) {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
@@ -45,21 +66,25 @@ function renderCardOfList(images) {
         }) => {
           return `
   <div class="photo-card">
-    <a href="${webformatURL}">
-      <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+    <a class="photo-card__link" href="${webformatURL}">
+      <img class="photo-card__img" src="${largeImageURL}" alt="${tags}" loading="lazy" />
     </a>
     <div class="info">
-      <p class="info-item">${likes}
+      <p class="info-item">
         <b>Likes</b>
+        ${likes}
       </p>
-      <p class="info-item">${views}
+      <p class="info-item">
         <b>Views</b>
+        ${views}
       </p>
-      <p class="info-item">${comments}
+      <p class="info-item">
         <b>Comments</b>
+        ${comments}
       </p>
-      <p class="info-item">${downloads}
+      <p class="info-item">
         <b>Downloads</b>
+        ${downloads}
       </p>
     </div>
   </div>`;
@@ -68,8 +93,27 @@ function renderCardOfList(images) {
       .join('');
 
     $refs.container.insertAdjacentHTML('beforeend', markup);
+
+    // if (images.hits.length >= images.totalHits) {
+    //   console.log(images.totalHits);
+    //   console.log(images.hits.length);
+    //   // $refs.loadButton.style.display = 'none';
+    //   Notify.warning(
+    //     "We're sorry, but you've reached the end of search results."
+    //   );
+    // }
+
+    if (images.totalHits < cardsApiService.per_page) {
+      $refs.loadButton.style.display = 'none';
+    }
   }
 
   const card = new SimpleLightbox('.gallery a');
   card.refresh();
 }
+
+function clearCardsContainer() {
+  $refs.container.innerHTML = '';
+}
+
+// function allLoadCards
